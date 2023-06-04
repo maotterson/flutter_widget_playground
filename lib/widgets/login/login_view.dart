baedb14eac53c8215775f10190f322e34a76ca92
maotterson <otterson.2@gmail.com>
2023-06-03 22:50:11 -0400

    change style of login button

--- lib/widgets/login/login_view.dart
+++ lib/widgets/login/login_view.dart
@@ -31,30 +31,34 @@ class _LoginViewState extends State<LoginView> {
 
     return Form(
       key: _formKey,
-      child: Center(
+      child: Padding(
+        padding: const EdgeInsets.all(25),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextFormField(
                 validator: validateUsername,
-                decoration:
-                    InputDecoration(border: inputBorder, hintText: 'Username')),
+                decoration: InputDecoration(
+                    border: inputBorder, labelText: 'Username')),
             TextFormField(
                 obscureText: true,
                 enableSuggestions: false,
                 autocorrect: false,
                 validator: validatePassword,
-                decoration:
-                    InputDecoration(border: inputBorder, hintText: 'Password')),
+                decoration: InputDecoration(
+                    border: inputBorder, labelText: 'Password')),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 16.0),
               child: ElevatedButton(
                 onPressed: () {
                   if (_formKey.currentState!.validate()) {
-                    // Process data.
+                    // todo
                   }
                 },
-                child: const Text('Submit'),
+                child: const Padding(
+                  padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
+                  child: Text('Log in'),
+                ),
               ),
             ),
           ],
